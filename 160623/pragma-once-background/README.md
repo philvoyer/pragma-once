1292d8f7df5a1ad1637ffa438762ba86d8cd594c
philvoyer <philvoyer@gmail.com>
2016-06-23 18:45:59 -0400

    #pragma-once-background
    Animation frame start by filling the canvas background with a clear
    color.

---
+++ 160623/pragma-once-background/README.md
@@ -0,0 +1,10 @@
+#\#pragma-once-background
+
+## Summary
+
+Animation frame start by filling the canvas background with a clear color.
+
+## Description
+
+A minimalistic program that fill the canvas background with a random clear color.
+Clear color change on click press.
---
+++ 160623/pragma-once-background/src/pragma_once_background/core.clj
@@ -0,0 +1,36 @@
+(ns pragma-once-background.core
+  (:require [quil.core :as quil]
+            [quil.middleware :as qm]))
+
+(defn random-color-rgb [color-range]
+  (into [] (take 3 (repeatedly #(rand-int color-range)))))
+
+(defn setup []
+  (quil/frame-rate 60)
+  {:clear-color (random-color-rgb 256) :press false})
+
+(defn update-scene [state]
+  (if (:press state)
+    (assoc state :clear-color (random-color-rgb 256))
+    state))
+
+(defn draw [state]
+  (let [[r g b] (:clear-color state)]
+    (quil/background r g b)))
+
+(defn click-press [state event]
+  (assoc state :press true))
+
+(defn click-release [state event]
+  (assoc state :press false))
+
+(quil/defsketch pragma-once-background
+  :title  "#pragma once background"
+  :size  [256 256]
+  :setup  setup
+  :draw   draw
+  :update update-scene
+  :mouse-pressed  click-press
+  :mouse-released click-release
+  :features      [:resizable]
+  :middleware    [qm/fun-mode])
