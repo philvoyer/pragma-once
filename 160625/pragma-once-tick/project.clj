8e02a896cdc475b509e6383b4c74485d6c3c508f
philvoyer <philvoyer@gmail.com>
2016-06-25 11:24:04 -0400

    #pragma-once-click
    Trigger an animation event after a fixed time delay.

---
+++ 160625/pragma-once-tick/project.clj
@@ -0,0 +1,7 @@
+(defproject pragma-once-tick "1.0"
+  :description "#pragma-once-tick"
+  :url "https://github.com/philvoyer/pragma-once"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [quil "2.4.0"]])
