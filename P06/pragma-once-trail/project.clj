adddbddcc20dcb2f63b08381d1496b919c447f95
philvoyer <philippe.voyer@ift.ulaval.ca>
2018-08-25 14:41:47 -0400

    dependencies upgrade

--- P06/pragma-once-trail/project.clj
+++ P06/pragma-once-trail/project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/philvoyer/pragma-once"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [quil "2.4.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [quil "2.7.1"]])
